b7c1db250ea2ac5865302e9b22bc898fb03e7502
umuhozalionel <muhozalionel@gmail.com>
2025-07-18 03:01:17 -0500

    Remove duplicate Header import

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,6 @@
 
 import Header from "@/components/Header"
 import RegistrationForm from "./registration-form"
-import Header from "@/components/Header"
 
 export default function RegisterPage() {
   return (
