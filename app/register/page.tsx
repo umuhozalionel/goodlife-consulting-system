9c12933a12afe0b19571ed769c22be994d809898
umuhozalionel <muhozalionel@gmail.com>
2025-07-18 02:39:26 -0500

    fix: correct import case for Header component in multiple pages

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "@/components/Header"
 import RegistrationForm from "./registration-form"
+import Header from "@/components/Header"
 
 export default function RegisterPage() {
   return (
